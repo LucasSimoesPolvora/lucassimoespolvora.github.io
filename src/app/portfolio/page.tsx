ead9bd6bda6db72aba7c1c68046e73307d483190
Lucas Simões Pólvora <122774951+LucasSimoesPolvora@users.noreply.github.com>
2024-11-11 14:10:51 +0100

    feat (bgImage): Put the image on the right with a linear gradient

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,29 +1,35 @@
 import Image from "next/image";
+import Inter from "next/font/google"
+
 
 export default function Home() {
   return (
-      <div className="relative w-full h-screen bg-black text-white overflow-hidden">
-      
+    <div className="relative w-full h-screen bg-black text-white overflow-hidden">
+
       {/* <p className="text-4xl font-reklame">Hello World</p> */}
-      <div className="absolute top-0 left-0 w-full h-full opacity-30 z-10">
+      <div className="absolute  top-0 right-0 w-4/6 h-full opacity-50">
         <Image
-          src="/bgImg.png" 
+          src="/bgImg.png"
           alt="Background Image"
           layout="fill"
           objectFit="cover"
           className="grayscale"
+          style={{
+            maskImage: "linear-gradient(to right, transparent, black)", // Fade effect on the image
+            WebkitMaskImage: "linear-gradient(to right, transparent, black)" // For cross-browser compatibility
+          }}
         />
       </div>
 
       {/* Text Overlay */}
       <div className="relative z-20 flex flex-col justify-center h-full px-8">
 
-      <h1 className="text-[10vw] font-iceland text-transparent bg-clip-text bg-gradient-to-br from-blue-500 via-aqua-500 to-green-500 leading-none uppercase">
+        <h1 className="text-[10vw] font-bold text-transparent bg-clip-text bg-gradient-to-br from-blue-500 via-aqua-500 to-green-500 leading-none uppercase">
           lucas
         </h1>
         <div className="max-w-md mt-8 space-y-4">
           <p className="text-lg font-iceland">
-            I'm a <span className="font-bold">developpement student</span> from Switzerland living in Lausanne, 
+            I'm a <span className="font-bold">developpement student</span> from Switzerland living in Lausanne,
             I am currently in the ETML.
           </p>
         </div>
