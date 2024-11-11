69d3f290292a9b22cdc669abf12bdd20acf8a888
Lucas Simões Pólvora <122774951+LucasSimoesPolvora@users.noreply.github.com>
2024-11-11 13:02:02 +0100

    fix(bgImage): Fix an issue with the bg image
    It wasn't displaying because it was the wrong file extension

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -3,11 +3,11 @@ import Image from "next/image";
 export default function Home() {
   return (
       <div className="relative w-full h-screen bg-black text-white overflow-hidden">
-      <p className="text-4xl font-reklame">Hello World</p>
-      {/* Background Image with Overlay */}
+      
+      {/* <p className="text-4xl font-reklame">Hello World</p> */}
       <div className="absolute top-0 left-0 w-full h-full opacity-30 z-10">
         <Image
-          src="/image.jpg" 
+          src="/bgImg.png" 
           alt="Background Image"
           layout="fill"
           objectFit="cover"
@@ -17,13 +17,14 @@ export default function Home() {
 
       {/* Text Overlay */}
       <div className="relative z-20 flex flex-col justify-center h-full px-8">
+
       <h1 className="text-[10vw] font-iceland text-transparent bg-clip-text bg-gradient-to-br from-blue-500 via-aqua-500 to-green-500 leading-none uppercase">
           lucas
         </h1>
         <div className="max-w-md mt-8 space-y-4">
           <p className="text-lg font-iceland">
-            I'm a <span className="font-bold">Developper</span> from Switzerland living in Lausanne, 
-            I am currently a student in the ETML. 
+            I'm a <span className="font-bold">developpement student</span> from Switzerland living in Lausanne, 
+            I am currently in the ETML.
           </p>
         </div>
       </div>
